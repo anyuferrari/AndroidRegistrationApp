4f84de1942fb87261f6840493fe7e8c856c44ed0
anyuferrari <anyuferrari@gmail.com>
2022-04-16 20:23:27 -0300

    Created an activity that takes three inputs and edits text in the lower section of the screen based on that input

--- app/src/main/java/com/example/registration/MainActivity.java
+++ app/src/main/java/com/example/registration/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.registration;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void register(View view) {
+        TextView nameOutput = findViewById(R.id.nameText);
+        EditText name = findViewById(R.id.nameInput);
+        String nameString = name.getText().toString();
+        nameOutput.setText("Name: " + nameString);
+
+        TextView lastNameOutput = findViewById(R.id.lastNameText);
+        EditText lastName = findViewById(R.id.lastNInput);
+        String lastNameString = lastName.getText().toString();
+        lastNameOutput.setText("Last name: " + lastNameString);
+
+        TextView emailOutput = findViewById(R.id.emailText);
+        EditText email = findViewById(R.id.emailInput);
+        String emailString = email.getText().toString();
+        emailOutput.setText("Email: " + emailString);
     }
 }
